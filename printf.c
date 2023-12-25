3df3b9c6cfc6ebf6c61c97e495debfdcf56f0f04
Michael Shambel <Michaelshambel10@gmail.com>
2023-12-25 04:53:35 -0800

    error 1

--- printf.c
+++ printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	check c[] = {
 		{"%c", printf_char}, {"%%", print_perc},
 		{"%d", print_dec}, {"%s", printf_string},
-		{"%i", print_d}, {"%b", print_bin}, {"%u", print_unsigned},
+		{"%i", print_d}, {"%u", print_unsigned},
 		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
 		{"%S", print_e_string}
 	};
